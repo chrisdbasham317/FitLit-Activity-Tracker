c7dc0a1f5da6c55f6819d617c18b941742a45458
Noah Gibson <noah.gibson99@gmail.com>
2019-08-20 18:37:27 -0600

    Add tests and functionality for average sleep and daily sleep

--- test/sleep-data-subset.js
+++ test/sleep-data-subset.js
@@ -1,7 +1,7 @@
 const sleepData = [
   {
     "userID": 1,
-    "date": "2019/06/15",
+    "date": "2019/06/14",
     "hoursSlept": 6.1,
     "sleepQuality": 2.2
   },
@@ -12,20 +12,20 @@ const sleepData = [
     "sleepQuality": 4.7
   },
   {
-    "userID": 3,
-    "date": "2019/06/15",
+    "userID": 2,
+    "date": "2019/06/16",
     "hoursSlept": 10.8,
     "sleepQuality": 4.7
   },
   {
-    "userID": 4,
-    "date": "2019/06/15",
+    "userID": 2,
+    "date": "2019/06/17",
     "hoursSlept": 5.4,
     "sleepQuality": 3
   },
   {
     "userID": 5,
-    "date": "2019/06/15",
+    "date": "2019/06/18",
     "hoursSlept": 4.1,
     "sleepQuality": 3.6
   }];
--- test/sleep-test.js
+++ test/sleep-test.js
@@ -2,13 +2,36 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const Sleep = require('../src/Sleep');
+const SleepRepository = require('../src/Sleep-repository');
 const userData = require('../test/sleep-data-subset');
 const userInfo = userData;
 
 describe('Sleep', () => {
+
+  let sleep, sleepRepository;
+
+  beforeEach(() => {
+    sleepRepository = new SleepRepository(userInfo);
+    sleep = new Sleep(sleepRepository.returnUserSleep(2));
+  });
+
   it('should be a function', () => {
     expect(Sleep).to.be.a('function');
+  });
+
+  it('should be able to get the users data', () => {
+    expect(sleep.data).to.deep.equal([userInfo[1], userInfo[2], userInfo[3]]);
+  });
+
+  it('should be able to calculate the daily amount of sleep', () => {
+    expect(sleep.calculateDailySleep('2019/06/15')).to.equal(7);
   })
 
-  it('should be able to get the users data', )
+  it('should be able to calculate average time slept per day', () => {
+    expect(sleep.calculateAverageSleep()).to.equal(8);
+  });
+
+  it('should be able to calculate the average sleep quality', () => {
+    expect(sleep.calculateAverageSleepQual()).to.equal(12.4);
+  });
 })
