6442f2b8c0476f90e95fa55827d0cd017005970b
Chris Basham <chrisdbasham0317@gmail.com>
2019-08-21 23:37:52 -0600

    Add methods for daily miles & minutes active

--- src/Activity.js
+++ src/Activity.js
@@ -1,6 +1,32 @@
-class Activity {
-  constructor() {
+const User = require('../src/user');
 
+class Activity {
+  constructor(activityObj) {
+    this.userActivity = activityObj;
+  }
+
+  sumActivty(arr) {
+    let total = arr.reduce((totalActivity, currentActivity) => {
+      return totalActivity += currentActivity;
+    }, 0);
+    return total;
+  }
+
+  getActivityTotal(passedDate, activity) {
+    let date = Date.parse(passedDate);
+    let thisDaysActivity = this.userActivity.filter(elem => Date.parse(elem.date) === date);
+    let totalActivity = thisDaysActivity.map(elem => elem[activity]);
+    return this.sumActivty(totalActivity);
+  }
+
+  getMilesWalkedDay(passedDate, activity, user) {
+    let distanceWalked = this.getActivityTotal(passedDate, activity);
+    return `${((distanceWalked * user.strideLength) / 5280).toFixed(2)}`;
+  }
+
+  getMinutesActiveDay(passedDate, activity) {
+    let totalMinutes = this.getActivityTotal(passedDate, activity);
+    return totalMinutes;
   }
 }
 
