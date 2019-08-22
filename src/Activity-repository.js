875dbe45e03c486e27590ba89510d6489bca0230
Chris Basham <chrisdbasham0317@gmail.com>
2019-08-21 22:18:26 -0600

    Add avg activity methods for activtyRepo class

--- src/Activity-repository.js
+++ src/Activity-repository.js
@@ -1,7 +1,27 @@
 class ActivityRepository {
-  constructor() {
-
+  constructor(data) {
+    this.usersData = data;
   }
+
+  returnUserActivityByID(userId) {
+    let userData = this.usersData.filter(user => user.userID === userId)
+    return userData;
+  }
+
+  sumArray(arr) {
+    let total = arr.reduce((totalActivity, currentActivity) => {
+      return totalActivity += currentActivity;
+    }, 0);
+    return total;
+  } 
+
+  getAvgActivity(passedDate, activity) {
+    let date = Date.parse(passedDate)
+    let thisDaysActivity = this.usersData.filter(elem => Date.parse(elem.date) === date);
+    let totalActivity = thisDaysActivity.map(elem => elem[activity]);
+    return this.sumArray(totalActivity) / totalActivity.length;
+  }
+  
 }
 
 
